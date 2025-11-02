d752e708a8f2fb96df995b7e365d37aff26d4896
Nguyen Kha Duong <nguyenkhaduong1010@gmail.com>
2025-11-02 18:10:07 +0700

    feat: add cland-format to check line

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+ * hello.c
+ *
+ *  Created on: November 02, 2025
+ *      Author: Nguyen Kha Duong
+ *      Contact via email: duong nguyen kha.daniel@gmail.com
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World!\n");
+
+  return 0;
+}
